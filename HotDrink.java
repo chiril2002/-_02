fe8ef57fdb5ba3d009475afd87c3fb2635103e34
kiril <ionhawk397@gmail.com>
2024-12-03 21:57:26 +0200

    HotDrink.java

---
+++ HotDrink.java
@@ -0,0 +1,22 @@
+public class HotDrink {
+    private String name;
+    private int volume;
+
+    public HotDrink(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "HotDrink{name='" + name + "', volume=" + volume + "ml}";
+    }
+}
